b3f7eba688cd6402f48eec507ee966e4ff018091
wangwhh <2356300214@qq.com>
2024-11-29 14:26:25 +0800

    try

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,24 @@
 <script setup lang="ts">
 import { ref, onMounted } from "vue";
-import { theme } from 'ant-design-vue';
 import refreshImage from './assets/refresh.png';
 import Divider from './assets/divider.png';
 import Arrow from './assets/arrow.png';
+import NotUsed from './assets/not_used.png';
 
 const isConfirmed = ref(false);
 const confirmRide = () => {
     isConfirmed.value = true;
+    qrcodeColor.value = "#aaaaaa";
 };
-
-const { useToken } = theme;
-const { token } = useToken();
 
 const currentTime = ref<string>("");
 
 const carTime = ref<string>("13:10");
 const start = ref<string>("玉泉校区教二南侧");
 const end = ref<string>("紫金港校区");
+const carId = ref<string>("教师9号班车");
 
+const qrcodeColor = ref<string>("#3381ff");
 // 实现实时更新当前时间
 const updateCurrentTime = (): void => {
   const now = new Date();
@@ -53,30 +53,43 @@ const getDay = (): string => {
     return `${month}月${day}日`;
 };
 
+const openModal = ref(false);
+const handleClick = () => {
+    openModal.value = true;
+};
+
+const handleOk = () => {
+    openModal.value = false;
+};
 </script>
 
 <template>
     <div id="app">
         <a-card :bordered="false" style="height:fit-content; position: relative;">
-            <p style="font-size: 25px; font-weight:450 ;text-align: left; margin-left: 10px; margin-top: 10px; margin-bottom: 5px;">{{getYearDay()}}</p>
-            <div style="display: flex; justify-content: space-between; align-items: center; margin: 15px; margin-top: 0;">
+            <p style="font-size: 25px; font-weight:450 ;text-align: left; margin-left: 10px; margin-top: 10px; margin-bottom: 5px;">{{getYearDay()}}
+                <img :src="NotUsed" alt="" style=" width: 50px; height: auto; margin-left: 0px;"/>
+            </p>
+            <div style="display: flex; justify-content: space-between; align-items: center; margin: 15px; margin-top: 0; margin-bottom: 10px;">
                 <div style="width: 33%;">
                     <p style="font-weight: bold; font-size: 30px; letter-spacing: -1px; margin-bottom: 0;">{{ carTime }}</p>
                     <p style="font-size: 18px; margin-bottom: 0;">{{ start }}</p>
                 </div>
-                <img :src="Arrow" alt="" style=" width: 150px; height: auto;"/>
+                <div>
+                    <p style="margin-bottom: 0; color: gray; font-weight: 400;">{{ carId }}</p>
+                    <img :src="Arrow" alt="" style=" width: 150px; height: auto; margin-top: 0;"/>
+                </div>
                 <p style="font-size: 18px; margin-bottom: 0;">{{ end }}</p>
             </div>
 
 
-            <img :src="Divider" alt="" style=" display: block; width: calc(100vw - 26px); height: auto;"/>
-            <img :src="refreshImage" alt="刷新" class="right-image" />
+            <img :src="Divider" alt="" style="display: block; width: calc(100vw - 26px); height: auto;"/>
+            <img :src="refreshImage" alt="刷新" class="right-image" @click="handleClick"/>
             <p class="time-text">{{ getDay() }}</p>
             <p class="time-text">{{ currentTime }}</p>
 
             <p class="green-text">当前时段趟次</p>
             <!-- 显示二维码 -->
-            <a-qrcode value="http://www.antdv.com" :color="token.colorInfoText" style="margin: auto; margin-bottom: 30px;"/>
+            <a-qrcode value="http://www.antdv.com" :color="qrcodeColor" style="margin: auto; margin-bottom: 10px; height: 150px; width: 150px;" :size="180"/>
 
             <!-- 确认按钮 -->
             <a-button type="primary" :disabled="isConfirmed" style="height: 48px; width: 90%; font-size: 20px;" @click="confirmRide"
@@ -88,6 +101,13 @@ const getDay = (): string => {
                 如有三次违约将禁止您使用此预约服务
             </a-typography-paragraph>
         </a-card>
+
+        <a-modal v-model:open="openModal" title="编辑信息" @ok="handleOk">
+            发车时间<a-input v-model:value="carTime" :placeholder="carTime" />
+            起点<a-input v-model:value="start" :placeholder="start" />
+            终点<a-input v-model:value="end" :placeholder="end" />
+            班车号<a-input v-model:value="carId" :placeholder="carId" />
+        </a-modal>
     </div>
 </template>
 
@@ -113,7 +133,7 @@ const getDay = (): string => {
     font-weight: 600;
     line-height: 40px;
     letter-spacing: 1px;
-    font-size: 33px;
+    font-size: 35px;
     margin-bottom: 10px;
     margin-top: 5px;
 }
